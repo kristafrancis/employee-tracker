ff26a7a3fbb2e64405f6ecd74154eae714fc5a31
kristafrancis721 <kristafrancis08@yahoo.com>
2022-08-29 12:36:22 -0400

    add route files, add code to connection.js

--- db/schema.sql
+++ db/schema.sql
@@ -2,22 +2,19 @@ DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS departments;
 
---create table for departments
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL 
 );
 
---create table for roles
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary VARCHAR(30) NOT NULL,
     department_id INTEGER,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
- -- create table for EMPLOYEES
 CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
